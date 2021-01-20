36d4bc12dc8fbd02e172d69ec868499052f64762
rokhuda <rokhuda@gmail.com>
2021-01-20 20:59:55 +0200

    modified

--- area_of_a_triangle.py
+++ area_of_a_triangle.py
@@ -1,5 +1,10 @@
+import math
+
 def area_of_a_triangle(a,b,c):
-    return 1/2 * (a + b + c)
+    semiperimeter = (a + b + c)/2
+    area = math.sqrt(semiperimeter*(semiperimeter-a)*(semiperimeter-b)*(semiperimeter-c))
+    return area
+
 
 
 print(area_of_a_triangle(3,4,5))

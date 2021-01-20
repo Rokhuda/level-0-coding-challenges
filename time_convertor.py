bf73c0d5296ba7de5e550c285f1095e9e36cc6ec
rokhuda <rokhuda@gmail.com>
2021-01-20 10:31:42 +0200

    added time convertor function

---
+++ time_convertor.py
@@ -0,0 +1,17 @@
+def time_convertor(num):
+
+    hours = num // 60
+    minutes = num % 60
+    
+    if hours <= 1 and minutes <= 1:
+        return (str(hours) + " hour, " + str(minutes) + " minute")
+    elif hours <= 1:
+        return (str(hours) + " hour, " + str(minutes) + " minutes")
+    elif minutes <= 1:
+        return (str(hours) + " hours, " + str(minutes) + " minute")
+    else:
+        return (str(hours) + " hours, " + str(minutes) + " minutes")
+
+
+print(time_convertor(120))
+

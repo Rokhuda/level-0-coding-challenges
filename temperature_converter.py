21490de38544de4af5384bd2c584e5d48b8c23be
rokhuda <rokhuda@gmail.com>
2021-01-20 09:24:30 +0200

    added temperature converter functions

---
+++ temperature_converter.py
@@ -0,0 +1,11 @@
+def fahrenheit_to_celsius(num):
+    return (num - 32) * 5/9
+
+
+def celsius_to_fahrenheit(num):
+    return num * (9/5) + 32
+
+
+print(fahrenheit_to_celsius(20))
+print(celsius_to_fahrenheit(0))
+

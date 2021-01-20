a9b0be4d89dc17836c779c3281822181f740b3d3
rokhuda <rokhuda@gmail.com>
2021-01-20 10:59:33 +0200

    added function that prints common letters

---
+++ common_letters.py
@@ -0,0 +1,8 @@
+def common_letters(word1, word2):
+    common = ""
+    for letter in word1:
+        if letter in word2:
+            common += ', ' + letter
+    return "Common letters: " + common
+
+print(common_letters("house","computers"))

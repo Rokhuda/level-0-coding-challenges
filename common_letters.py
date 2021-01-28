dac5bb2086427bd46268c437d52f06916422433a
rokhuda <rokhuda@gmail.com>
2021-01-28 13:58:49 +0200

    cleared dedundancy

--- common_letters.py
+++ common_letters.py
@@ -2,7 +2,7 @@ def common_letters(word1, word2):
     common = ""
     for letter in word1:
         if letter in word2:
-            common += ', ' + letter
+            common += letter + ', ' 
     return "Common letters: " + common
 
 print(common_letters("house","computers"))

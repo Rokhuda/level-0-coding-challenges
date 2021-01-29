123119f02d9221aff076a019d82af4345478c4ec
rokhuda <rokhuda@gmail.com>
2021-01-29 08:15:05 +0200

    enabled 0 input to be singular

--- time_convertor.py
+++ time_convertor.py
@@ -3,7 +3,7 @@ def time_convertor(num):
     hours = num // 60
     minutes = num % 60
     
-    if hours <= 1 and minutes <= 1:
+    if hours < 1 and minutes < 1:
         return (str(hours) + " hour, " + str(minutes) + " minute")
     elif hours <= 1:
         return (str(hours) + " hour, " + str(minutes) + " minutes")
@@ -13,5 +13,5 @@ def time_convertor(num):
         return (str(hours) + " hours, " + str(minutes) + " minutes")
 
 
-print(time_convertor(120))
+print(time_convertor(0))
 

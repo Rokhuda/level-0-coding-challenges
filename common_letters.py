3ffdf9c1f21bad3a437b6de94a25ff0eabdf606e
rokhuda <rokhuda@gmail.com>
2021-01-29 07:45:26 +0200

    removed last comma

--- common_letters.py
+++ common_letters.py
@@ -3,6 +3,7 @@ def common_letters(word1, word2):
     for letter in word1:
         if letter in word2:
             common += letter + ', ' 
-    return "Common letters: " + common
+    common = "Common letters: " + common
+    return common[:-2]
 
 print(common_letters("house","computers"))

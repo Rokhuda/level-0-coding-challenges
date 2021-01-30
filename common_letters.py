76a2bf394ff6020ed5519d972d19851914297b92
rokhuda <rokhuda@gmail.com>
2021-01-30 11:50:14 +0200

    modified to use join

--- common_letters.py
+++ common_letters.py
@@ -2,8 +2,7 @@ def common_letters(word1, word2):
     common = ""
     for letter in word1:
         if letter in word2:
-            common += letter + ', ' 
-    common = "Common letters: " + common
-    return common[:-2]
+            common += letter 
+    return "Common letters: " + ','.join(common)
 
 print(common_letters("house","computers"))
